b2b21b5d75ed02ad78b47f1d0d48eb388faa1bfe
TheWraithxVN <76970194+TheWraithxVN@users.noreply.github.com>
2021-01-05 21:10:43 +0100

    Add files via upload

---
+++ prg.c
@@ -0,0 +1,29 @@
+
+int main
+printf ("Hello World");
+printf("%f", 5.0 + 8.0)
+
+int i = 1
+for (i = 1; i <= 5; i++);
+printf("I am the best");
+
+int max (int num1, int num2);
+int result; 
+if (num1 > num2);
+result = num1;
+else 
+result = num2;
+
+
+int max (int num1, int num2, int num3)
+int result;
+
+if (num1 >= num2 && num1 >= num3)
+result = num1;
+else if(num2 >= num1 && num2 >= num3)
+result = num2;
+else 
+result = num3
+
+int main() 
+printf("%d", max(1,2,3));
